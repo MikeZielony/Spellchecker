20cf8e47725e131b363dd3ae90468a4fb58e16ca
MikeZielony <51535787+MikeZielony@users.noreply.github.com>
2020-09-30 13:52:08 +0200

    New Branch

--- Spellchecker.js
+++ Spellchecker.js
@@ -1,3 +1,4 @@
+"use strict";
 var words = ["lake", "lock", "lama", "lose", "luck", "like", "soke", "siwe", "sike"];
 var word = "lakeloek";
 var alfabhet = 'abcdefghijklmnopqrstuvwxyz';
@@ -9,56 +10,93 @@ function isWordinList(word) {
         }
     }
 }
-function isOneLetterError(word) {
+function putLetterBetween(word) {
     for (var i = 0; i < word.length; i++) {
         for (var j = 0; j < alfabhet.length; j++) {
-            var newWord = word.replace(word.charAt(i), alfabhet.charAt(j));
-            if (isWordinList(newWord) == true) {
-                results.push(newWord);
-            }
-            console.log(word);
+            var newWord = "";
+            newWord += alfabhet.charAt(j) + word.charAt(i);
+            console.log(newWord);
         }
     }
-}
-function isLettersreplaced(word) {
-    var _a;
-    for (var i = 0; i < word.length - 1; i++) {
-        var arra = word.split("");
-        _a = [arra[i + 1], arra[i]], arra[i] = _a[0], arra[i + 1] = _a[1];
+    function isOneLetterError(word) {
+        for (var i = 0; i < word.length; i++) {
+            for (var j = 0; j < alfabhet.length; j++) {
+                var newWord = word.replace(word.charAt(i), alfabhet.charAt(j));
+                if (isWordinList(newWord) == true) {
+                    results.push(newWord);
+                }
+                console.log(word);
+            }
+        }
+    }
+    function isLettersreplaced(word) {
+        var _a;
+        for (var i = 0; i < word.length - 1; i++) {
+            var arra = word.split("");
+            _a = [arra[i + 1], arra[i]], arra[i] = _a[0], arra[i + 1] = _a[1];
+            var newWord = '';
+            for (var i_1 = 0; i_1 < arra.length; i_1++) {
+                newWord += arra[i_1];
+                if (isWordinList(newWord) == true) {
+                    results.push(newWord);
+                }
+            }
+            console.log(newWord);
+        }
+    }
+    function separateWords(word) {
         var newWord = '';
-        for (var i_1 = 0; i_1 < arra.length; i_1++) {
-            newWord += arra[i_1];
+        // for (let i: number = 0; i < word.length - 1; i++) {
+        //     newWord += word.charAt(i);
+        //     console.log(newWord);
+        //     if (isWordinList(newWord) == true) {
+        //         results.push(newWord);
+        //     }
+        //     console.log(newWord);
+        // }
+        var i = 0;
+        while (i < word.length) {
+            newWord += word.charAt(i);
+            console.log(newWord);
+            var newString = word.substring(i + 1, word.length);
+            if ((isWordinList(newWord) == true) && (isWordinList(newString) == true)) {
+                results.push(newWord);
+                // let newString = word.substring(i+1,word.length);
+                results.push(newString);
+            }
+            console.log(results);
+            i += 1;
+        }
+    }
+    function findWordsFromList(word) {
+        var newWord = '';
+        // for (let i: number = 0; i < word.length - 1; i++) {
+        //     newWord += word.charAt(i);
+        //     console.log(newWord);
+        //     if (isWordinList(newWord) == true) {
+        //         results.push(newWord);
+        //     }
+        //     console.log(newWord);
+        // }
+        var i = 0;
+        while (i < word.length) {
+            newWord += word.charAt(i);
+            // console.log(newWord);
             if (isWordinList(newWord) == true) {
                 results.push(newWord);
+                var newString = word.substring(i + 1, word.length);
+                if (isWordinList(newString) == true) {
+                    results.push(newString);
+                }
             }
+            console.log(results);
+            i += 1;
         }
-        console.log(newWord);
     }
+    //console.log(isOneLetterError(word));
+    //   console.log(findWordsFromList(word));
+    //    console.log(results);
+    //console.log(isOneLetterError(word));
+    //    console.log(results);
 }
-function separateWords(word) {
-    var newWord = '';
-    // for (let i: number = 0; i < word.length - 1; i++) {
-    //     newWord += word.charAt(i);
-    //     console.log(newWord);
-    //     if (isWordinList(newWord) == true) {
-    //         results.push(newWord);
-    //     }
-    //     console.log(newWord);
-    // }
-    var i = 0;
-    while (i < word.length) {
-        newWord += word.charAt(i);
-        console.log(newWord);
-        var newString = word.substring(i + 1, word.length);
-        if ((isWordinList(newWord) == true) && (isWordinList(newString) == true)) {
-            results.push(newWord);
-            // let newString = word.substring(i+1,word.length);
-            results.push(newString);
-        }
-        console.log(results);
-        i += 1;
-    }
-}
-//console.log(isOneLetterError(word));
-console.log(separateWords(word));
-console.log(results);
+putLetterBetween("lke");
--- Spellchecker.ts
+++ Spellchecker.ts
@@ -11,90 +11,103 @@ function isWordinList (word: string) {
         }
     }
 }
-function isOneLetterError (word: string) {
-    for(let i:number=0; i<word.length;i++) {
-        for(let j:number=0; j<alfabhet.length; j++){
-           let  newWord: string = word.replace(word.charAt(i),alfabhet.charAt(j));
-            if(isWordinList(newWord) == true) {
-                results.push(newWord);
 
-            }
-            console.log(word);
+function putLetterBetween(word: string) {
+    for (let i: number = 0; i < word.length; i++) {
+        for (let j: number = 0; j < alfabhet.length; j++) {
+            let newWord: string = "";
+            newWord += alfabhet.charAt(j) + word.charAt(i);
+            console.log(newWord);
         }
     }
-}
-function isLettersreplaced (word:string) {
-    for(let i:number=0; i<word.length-1;i++) {
-        let arra: string[] = word.split("");
-        [arra[i],arra[i+1]]=[arra[i+1],arra[i]];
-        let newWord = '';
-        for(let i:number=0; i<arra.length;i++){
-            newWord += arra[i];
-            if(isWordinList(newWord) == true) {
-                results.push(newWord);
-            }
-        }      console.log(newWord);
-    }
-}
 
-function separateWords(word: string) {
-    let newWord = '';
-    // for (let i: number = 0; i < word.length - 1; i++) {
-    //     newWord += word.charAt(i);
-    //     console.log(newWord);
-    //     if (isWordinList(newWord) == true) {
-    //         results.push(newWord);
-    //     }
-    //     console.log(newWord);
-    // }
-    let i:number = 0;
-    while(i<word.length ){
-        newWord += word.charAt(i);
+    function isOneLetterError(word: string) {
+        for (let i: number = 0; i < word.length; i++) {
+            for (let j: number = 0; j < alfabhet.length; j++) {
+                let newWord: string = word.replace(word.charAt(i), alfabhet.charAt(j));
+                if (isWordinList(newWord) == true) {
+                    results.push(newWord);
+
+                }
+                console.log(word);
+            }
+        }
+    }
+
+    function isLettersreplaced(word: string) {
+        for (let i: number = 0; i < word.length - 1; i++) {
+            let arra: string[] = word.split("");
+            [arra[i], arra[i + 1]] = [arra[i + 1], arra[i]];
+            let newWord = '';
+            for (let i: number = 0; i < arra.length; i++) {
+                newWord += arra[i];
+                if (isWordinList(newWord) == true) {
+                    results.push(newWord);
+                }
+            }
             console.log(newWord);
-        let newString = word.substring(i+1,word.length);
-            if ((isWordinList(newWord) == true) && ( isWordinList(newString) == true)) {
+        }
+    }
+
+    function separateWords(word: string) {
+        let newWord = '';
+        // for (let i: number = 0; i < word.length - 1; i++) {
+        //     newWord += word.charAt(i);
+        //     console.log(newWord);
+        //     if (isWordinList(newWord) == true) {
+        //         results.push(newWord);
+        //     }
+        //     console.log(newWord);
+        // }
+        let i: number = 0;
+        while (i < word.length) {
+            newWord += word.charAt(i);
+            console.log(newWord);
+            let newString = word.substring(i + 1, word.length);
+            if ((isWordinList(newWord) == true) && (isWordinList(newString) == true)) {
                 results.push(newWord);
-               // let newString = word.substring(i+1,word.length);
+                // let newString = word.substring(i+1,word.length);
                 results.push(newString);
             }
             console.log(results);
-            i+=1;
+            i += 1;
         }
 
-}
-
-function findWordsFromList(word: string) {
-    let newWord = '';
-    // for (let i: number = 0; i < word.length - 1; i++) {
-    //     newWord += word.charAt(i);
-    //     console.log(newWord);
-    //     if (isWordinList(newWord) == true) {
-    //         results.push(newWord);
-    //     }
-    //     console.log(newWord);
-    // }
-    let i:number = 0;
-    while(i<word.length ){
-        newWord += word.charAt(i);
-        // console.log(newWord);
-        if (isWordinList(newWord) == true) {
-            results.push(newWord);
-            let newString = word.substring(i+1,word.length);
-            if (isWordinList(newString) == true) {
-                results.push(newString);
-            }
-        }
-        console.log(results);
-        i+=1;
     }
 
+    function findWordsFromList(word: string) {
+        let newWord = '';
+        // for (let i: number = 0; i < word.length - 1; i++) {
+        //     newWord += word.charAt(i);
+        //     console.log(newWord);
+        //     if (isWordinList(newWord) == true) {
+        //         results.push(newWord);
+        //     }
+        //     console.log(newWord);
+        // }
+        let i: number = 0;
+        while (i < word.length) {
+            newWord += word.charAt(i);
+            // console.log(newWord);
+            if (isWordinList(newWord) == true) {
+                results.push(newWord);
+                let newString = word.substring(i + 1, word.length);
+                if (isWordinList(newString) == true) {
+                    results.push(newString);
+                }
+            }
+            console.log(results);
+            i += 1;
+        }
+
+    }
+
+//console.log(isOneLetterError(word));
+ //   console.log(findWordsFromList(word));
+//    console.log(results);
+
+//console.log(isOneLetterError(word));
+//    console.log(results);
 }
 
-//console.log(isOneLetterError(word));
-console.log(findWordsFromList(word));
-console.log(results);
-
-//console.log(isOneLetterError(word));
-console.log(separateWords(word));
-console.log(results);
-
+putLetterBetween("lke");
